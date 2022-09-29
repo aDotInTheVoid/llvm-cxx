bb23ff7f4683d1e13d8be0aa58927fee220318e2
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-09-29 23:47:53 +0100

    Use clang-format

--- src/llvm-cxx.hh
+++ src/llvm-cxx.hh
@@ -2,24 +2,24 @@
 
 #include <memory>
 
-#include "llvm/IR/LLVMContext.h"
+#include "llvm/ADT/APFloat.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Value.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/ADT/APFloat.h"
-#include "llvm/IR/Function.h"
 
 #include "rust/cxx.h"
 
 using llvm::LLVMContext;
-using std::unique_ptr;
 using llvm::Module;
+using std::unique_ptr;
 
 class IRBuilder {
-public:
+  public:
     llvm::IRBuilder<> builder_;
-    IRBuilder(LLVMContext &C): builder_(C) {}  
+    IRBuilder(LLVMContext &C) : builder_(C) {}
 };
 
 unique_ptr<LLVMContext> new_context();
